93ba6c8da8e00c303f05ac261bdeada6d6052e8b
Juanje Ojeda <juanje.ojeda@gmail.com>
2012-04-13 05:47:04 +0200

    '.tar' was missing from the tarball's url

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,6 +5,6 @@ default['mosh']['install_type'] = case node['platform']
                                     "package"
                                   end
 default['mosh']['version'] = "1.1.3"
-default['mosh']['source_url'] = "https://github.com/downloads/keithw/mosh/mosh-#{node['mosh']['version']}.gz"
+default['mosh']['source_url'] = "https://github.com/downloads/keithw/mosh/mosh-#{node['mosh']['version']}.tar.gz"
 default['mosh']['source_checksum'] = "53234667e53625791ca43ced1ec43834cbd86a019c67ce5e4bd65556113c6eee"
 default['mosh']['configure_flags'] = []
