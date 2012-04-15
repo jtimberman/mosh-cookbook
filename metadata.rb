7ae3e9e93e1ef60b311a138ce3b7b25ff365c673
jtimberman <joshua@opscode.com>
2012-04-14 21:55:33 -0600

    increment version for development

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cookbooks@housepub.org"
 license          "Apache 2.0"
 description      "Installs/Configures mosh"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.2.1"
 
 %w{ debian ubuntu mac_os_x mac_os_x_server redhat centos scientific amazon fedora gentoo arch }.each do |os|
   supports os
