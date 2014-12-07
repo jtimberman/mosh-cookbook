e2ce9e18b9ec222a4355fe55c698eca1778bbbeb
jtimberman <joshua@chef.io>
2014-12-06 18:19:55 -0700

    add url metadata for supermarket

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'cookbooks@housepub.org'
 license          'Apache 2.0'
 description      'Installs mosh'
 version          '0.3.1'
+source_url       'https://github.com/jtimberman/mosh-cookbook'
+issues_url       'https://github.com/jtimberman/mosh-cookbook/issues'
 
 %w{ debian ubuntu mac_os_x mac_os_x_server redhat centos scientific amazon fedora gentoo arch }.each do |os|
   supports os
