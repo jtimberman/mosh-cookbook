0b81184dedd6d817a2b01546d2176523181d63cb
Brian Hays <brian.hays@gmail.com>
2017-04-29 19:46:13 -0400

    update maintainer info and FC066 chef_version check

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name             'mosh'
-maintainer       'Joshua Timberman'
-maintainer_email 'cookbooks@housepub.org'
+maintainer       'Brian Hays'
+maintainer_email 'brian.hays@gmail.com'
 license          'Apache 2.0'
 description      'Installs mosh'
-version          '0.4.0'
+version          '0.5.0'
 
 %w{ debian ubuntu mac_os_x mac_os_x_server redhat centos scientific amazon fedora gentoo arch }.each do |os|
   supports os
@@ -17,3 +17,4 @@ unless defined?(Ridley::Chef::Cookbook::Metadata)
   source_url       'https://github.com/jtimberman/mosh-cookbook'
   issues_url       'https://github.com/jtimberman/mosh-cookbook/issues'
 end
+chef_version '>= 12.1' if respond_to?(:chef_version)
